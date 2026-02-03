f2c2aafc411c3d6231d5b42ede7162db3ceedb86
Gabriele Pintus <gabrielegpintus@gmail.com>
2026-02-03 16:45:16 +0100

    Mettendo in ordine le idee

--- preimage_sampling/models/classifiers.py
+++ preimage_sampling/models/classifiers.py
@@ -71,14 +71,18 @@ class MNISTClassifier(nn.Module):
     def __init__(self, num_classes=10):
         super(MNISTClassifier, self).__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=2, padding=2),
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.Conv2d(16, 16, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
             nn.Flatten(),
+            
             nn.ReLU(),
             nn.Linear(32 *2 *2, 10)
         )
--- src/network.py
+++ src/network.py
@@ -27,14 +27,18 @@ class SimpleCNNClassifier(nn.Module):
     def __init__(self):
         super(SimpleCNNClassifier, self).__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=2, padding=2),
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.Conv2d(16, 16, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
             nn.Flatten(),
+            
             nn.ReLU(),
             nn.Linear(32 *2 *2, 10)
         )
